807886b261ede24ae62a7371b1ec3bc19ab4f86a
fahrimert <fmtunckaya@gmail.com>
2026-02-13 01:41:52 +0300

    test: pipeline test

--- src/hello-world-app/app.py
+++ src/hello-world-app/app.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 @app.route("/")
 def hello():
     logger.info("Hello isteği başarıyla karşılandı.")
-    return "Hello World! Pipeline Test Ediliyor - V1"
+    return "Hello World! Pipeline Test Ediliyor - V2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
